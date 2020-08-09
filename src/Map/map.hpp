14797ccbac5d8450df7dfc3017f4e96c81b0b696
Luc-Ank <luas@anki.fr>
2020-08-10 00:35:00 +0200

    map hpp

---
+++ src/Map/map.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <unistd.h>
+#include <cmath>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+using namespace sf;
+
+#define screen_W 1024
+#define screen_H 768
+#define nbTile_W 32
+#define nbTile_H 24
+
+class Map
+{
+    public:
+        Map();
+
+        void loadMap(std::string filename);
+        void drawMap(int layer, RenderWindow &window);
+        void changeLevel(std::string filename);
+
+    protected:
+        int tile1[nbTile_H][nbTile_W];
+        int tile2[nbTile_H][nbTile_W];
+        int tile3[nbTile_H][nbTile_W];
+
+        Texture tileSetTexture;
+        Sprite tileSet;
+};
